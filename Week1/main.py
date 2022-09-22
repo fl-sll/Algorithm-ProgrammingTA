426b2c4f61460340fec751445c0fdccd7a592cf9
fl-sll <edwardalvin@gmail.com>
2022-09-22 22:01:00 +0700

    first commit

---
+++ Week1/main.py
@@ -0,0 +1,82 @@
+# Variables
+
+
+
+#! data types
+
+
+
+#? operators
+
+
+#? increment and decrement
+
+
+#? overloaded operators
+
+
+#? augmented assignment operators
+
+
+'''
+Write instructions/statements that swaps the values of x and y
+'''
+
+
+#? conditional operators
+
+
+##? printing
+
+
+#? printing data types
+
+
+#? format printing
+
+
+#? user input
+
+
+#? specify type of user input
+
+
+#? if, else and elif
+
+
+#? and & or
+
+
+#? Nested if
+
+
+
+#? for loop
+
+
+#? while loop
+
+
+#? Lists
+
+
+#? List operations
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Write a program that reads the three edges of a triangle and computes the perimeter if the
+input is valid. The input is valid if the sum of every pair of two edges is greater than the
+remaining edge. Otherwise (i.e. else) print a message stating that the input is invalid and the
+perimeter cannot be calculated. (Note: This question does NOT require further input
+validation.)
+'''
