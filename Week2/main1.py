37c6c789978fcdb71496d1f9506b373fdbafe179
fl-sll <edwardalvin@gmail.com>
2022-09-28 16:42:55 +0700

    "first commit"

---
+++ Week2/main1.py
@@ -0,0 +1,55 @@
+
+#! git commands
+#// initialize repo
+
+#// adding file
+
+#// checking status
+
+#// committing
+
+#// pushing
+
+
+#? for loop
+
+
+#? while loop
+
+
+#? lists
+#// ordered, indexed, mutable, allows duplicates
+
+#// Accessing list elements
+
+#// Changing list elements
+
+#// Adding list elements
+
+#// Removing list items
+
+
+#! tuples
+#// ordered, indexed, immutable, allows duplicates
+
+#// Changing tuple elements
+
+#// Adding tuple elements
+
+#// Removing tuple elements
+
+
+#! sets
+#// unordered, unindexed, *immutabale, no duplicates
+
+
+#! dictionaries
+#// *ordered, unindexed, mutable, no duplicates
+
+#// Accessing dictionary elements
+
+#// Changing dictionary elements
+
+#// Adding dictionary elements
+
+#// Removing dictionary elementsex
