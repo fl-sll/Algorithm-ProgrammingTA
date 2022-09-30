daeaaf506a4b2839a058f32132c0e841e1aab1a0
fl-sll <edwardalvin@gmail.com>
2022-09-30 17:01:12 +0700

    first commit

---
+++ Week2/assignment.md
@@ -0,0 +1,13 @@
+Write a program for a guessing game. Tell the user to pick a number from 1 - 100 (you can put a lower upper range). Take the user's input (int) and the program will tell if the user's guess is correct. If the user's input is not correct, tell them if the guess was too high or too low. You may incorporate a while loop, if you already understand, to let the program run until a guess is correct.
+
+Hint: To get a random integer, you can utilize:
+
+```python
+import random
+randomint = random.randint(1,100) # In this case the range is from 1 to 100
+```
+Push the file to a Github Repo (it can be where you store your code) and send me the link to the specific file / repo through WA (081805188989) or discord (alvin#1649).
+
+** Due next Saturday (8th October 2022) @ 23.59! **
+
+** Good Luck and Have fun !!! **
--- Week2/main1.py
+++ Week2/main1.py
@@ -11,48 +11,80 @@
 # // pushing
 
 
-#? for loop
-list1 = ["hello", "world"]
-for i in list1:
-    print(i)
-
-
-#? while loop
-
 
 #? lists
 #// ordered, indexed, mutable, allows duplicates
-
+x = [1, "hello", 2.5];
+# print(len(x))
+# print(x)
 #// Accessing list elements
-
+# print(x[-2])
 #// Changing list elements
-
+# x[1] = "world"
+# print(x)
 #// Adding list elements
+# x.append("world")
+# x.insert(1, "world")
+# y = [2,3,4]
+# x.extend(y)
+# print(x)
 
 #// Removing list items
+# x.pop()
+# x.pop(1)
+# x.remove("hello")
+# print(x)
 
 
-#! tuples
+#? tuples
 #// ordered, indexed, immutable, allows duplicates
+# myTuple = (1,"hello",2.5)
+# # print(myTuple)
+# #// Changing tuple elements
+# tupleList = list(myTuple)
+# tupleList[1] = "World"
+# myTuple = tuple(tupleList)
+# print(myTuple)
 
-#// Changing tuple elements
 
-#// Adding tuple elements
-
-#// Removing tuple elements
-
-
-#! sets
+#? sets
 #// unordered, unindexed, *immutabale, no duplicates
+mySet = {1, "hello", 2.5}
 
 
-#! dictionaries
+#? dictionaries
 #// *ordered, unindexed, mutable, no duplicates
-
+thisDict = {
+    "car" : "Honda",
+    "year" : "2000",
+    "city" : "jakarta"
+}
+# print(thisDict)
 #// Accessing dictionary elements
+# print(thisDict["car"])
+# print(thisDict["year"])
+# print(thisDict["city"])
 
 #// Changing dictionary elements
+# thisDict["car"] = "Toyota"
+# thisDict.update({"year": "2010"})
+# print(thisDict)
 
 #// Adding dictionary elements
+# thisDict["color"] = "yellow"
+# print(thisDict)
 
-#// Removing dictionary elementsex
+# #// Removing dictionary elements
+# thisDict.pop("color")
+# print(thisDict)
+
+# ? for loop
+# for i in x:
+#     print(i)
+
+
+# ? while loop
+index = 0
+while index <= 2:
+    print(x[index])
+    index += 1
