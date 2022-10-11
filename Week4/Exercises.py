24ea53abaec218bdaa88b0a9995f814493dc476c
fl-sll <edwardalvin@gmail.com>
2022-10-11 13:06:15 +0700

    added exercises for week 4

---
+++ Week4/Exercises.py
@@ -0,0 +1,23 @@
+'''
+1. Write a program to add two lists index-wise. Create a new list that contains
+the 0th index item from both the list, then the 1st index item, and so on till
+the last element. any leftover items will get added at the end of the new list.
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+output = ['My', 'name', 'is', 'Kelly']
+
+2. Given a two Python list. Write a program to iterate both lists simultaneously
+and display items from list1 in original order and items from list2 in reverse order.
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+output = 
+10 400
+20 300
+30 200
+40 100
+
+3. Concatenate two lists in the following order
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+output = ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+'''
