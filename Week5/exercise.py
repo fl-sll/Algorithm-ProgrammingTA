1dc31a38f7d5b075e12bf1cac3e39bd4a37ad7eb
fl-sll <edwardalvin@gmail.com>
2022-10-21 16:40:44 +0700

    added due date

--- Week5/exercise.py
+++ Week5/exercise.py
@@ -4,4 +4,5 @@ Basically everything u have learnt so far. NO CHEATING OR GOOGLING
 OR ASKING ALYSHA AND CHELLSHE!!!
 
 push ur answers to a repo and WA or Discord the link to me
+DUE DATE: 22/10/2022 - 11.59PM
 '''
