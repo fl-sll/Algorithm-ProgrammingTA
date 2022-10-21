cd446ec0d33167eaf47dcfa4f7c5008d56ba3cb0
fl-sll <edwardalvin@gmail.com>
2022-10-21 16:38:45 +0700

    update exercise

--- Week5/exercise.py
+++ Week5/exercise.py
@@ -2,4 +2,6 @@
 Create a calculator program using functions, loops, if and else.
 Basically everything u have learnt so far. NO CHEATING OR GOOGLING
 OR ASKING ALYSHA AND CHELLSHE!!!
+
+push ur answers to a repo and WA or Discord the link to me
 '''
