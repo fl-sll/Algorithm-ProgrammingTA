b3f96e9e933f45ce67b87014f1ee83e548466278
fl-sll <edwardalvin@gmail.com>
2022-12-02 16:54:09 +0700

    first commit

---
+++ Week10/pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // int age = 43;
+    // int *ptr = &age;
+
+    // printf("%d\n", age);
+    // printf("%p\n", ptr);
+    // printf("%d", *ptr);
+
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int *ptr = arr;
+
+    printf("%d\n", arr);
+    printf("%p", ptr);
+
+    //// Write a program in C to show the basic declaration of pointer
+
+}
